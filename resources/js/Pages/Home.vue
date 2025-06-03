a5a49a7bf90c0d0909a1d3f50d10813b354aa832
Daniel <devops@orpot.com>
2025-06-03 13:19:44 -0600

    Intertia + VUEJS limpios

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-4 text-center">
+    <h1 class="text-3xl font-bold">AcademyBallon Iniciado 🎈</h1>
+    <p>¡Vue + Inertia + Laravel funcionando! trabajando juntos
+    </p>
+  </div>
+</template>
